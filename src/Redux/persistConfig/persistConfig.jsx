dda95dca7d3c590c2aecb1e7b926433ea62cc988
Masood Abbas <masoodabbas421@gmail.com>
2025-12-01 17:37:49 +0500

    presit data

--- src/Redux/persistConfig/persistConfig.jsx
+++ src/Redux/persistConfig/persistConfig.jsx
@@ -2,21 +2,44 @@ import storage from "redux-persist/lib/storage";
 import { persistReducer } from "redux-persist";
 import { combineReducers } from "@reduxjs/toolkit";
 
-import authReducer from "../authSlice/authSlice"
-import productReducer from "../producttSlice/productSlice"
-import cartReducer from "../cartSlice/cartSlice"
+import authReducer from "../authSlice/authSlice";
+import productReducer from "../producttSlice/productSlice";
+import cartReducer from "../cartSlice/cartSlice";
 
-
-const persistConfig = {
-  key: "root",
+// user ONLY
+const userPersistConfig = {
+  key: "user",
   storage,
-  whitelist: ["auth", "cart"], 
+  whitelist: ["user"],
+};
+
+// accessToken ONLY
+const accessTokenPersistConfig = {
+  key: "accessToken",
+  storage,
+  whitelist: ["accessToken"],
+};
+
+// refreshToken ONLY
+const refreshTokenPersistConfig = {
+  key: "refreshToken",
+  storage,
+  whitelist: ["refreshToken"],
+};
+
+// cart
+const cartPersistConfig = {
+  key: "cart",
+  storage,
+  whitelist: ["items", "totalQuantity", "totalPrice"],
 };
 
 const rootReducer = combineReducers({
-  auth: authReducer,
+  user: persistReducer(userPersistConfig, authReducer),
+  accessToken: persistReducer(accessTokenPersistConfig, authReducer),
+  refreshToken: persistReducer(refreshTokenPersistConfig, authReducer),
+  cart: persistReducer(cartPersistConfig, cartReducer),
   products: productReducer,
-  cart: cartReducer,
 });
 
-export default persistReducer(persistConfig, rootReducer);
+export default rootReducer;
